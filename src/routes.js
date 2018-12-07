5f69833d0a5106be6a9920c7658ca4a7680ddda3
Arthur <arr90rj@gmail.com>
2018-12-07 01:42:48 -0200

    ajuste para renderizar os componentes

--- src/routes.js
+++ src/routes.js
@@ -1,17 +1,15 @@
 import React from 'react';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import { Switch, Route, withRouter  } from 'react-router-dom';
 import Main from './pages/main';
 import Product from './pages/product';
 import NewProduct from './pages/ProductForm';
 
 const Routes = () => (
-    <BrowserRouter>
-        <Switch>
-            <Route exact path="/" component={Main}></Route>
-            <Route path="/product/new" component={NewProduct}></Route>
-            <Route path="/product" component={Product}></Route>
-        </Switch>
-    </BrowserRouter>
+    <Switch>
+        <Route path="/" component={withRouter(Main)} exact></Route>
+        <Route path="/product/new" component={withRouter(NewProduct)}></Route>
+        <Route path="/product" component={withRouter(Product)}></Route>
+    </Switch>
 );
 
 export default Routes;
